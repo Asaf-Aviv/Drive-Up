c3e6bab3b3f63553c6582f902210d8abaec554af
Asaf Aviv <asafaviv89@gmail.com>
2019-08-30 18:23:56 +0300

    added redux and GA

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
 import App from './App';
+import configureStore from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactGA.initialize('');
+
+const history = createBrowserHistory();
+
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname });
+  ReactGA.pageview(location.pathname);
+});
+
+// eslint-disable-next-line import/prefer-default-export
+export const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'),
+);
