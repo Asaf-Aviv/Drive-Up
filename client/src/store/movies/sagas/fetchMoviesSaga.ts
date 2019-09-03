ccb5e257e52ca0909198bf460bbc62dc7027c38b
Asaf Aviv <asafaviv89@gmail.com>
2019-09-03 13:07:06 +0300

    strong type redx-saga put

--- client/src/store/movies/sagas/fetchMoviesSaga.ts
+++ client/src/store/movies/sagas/fetchMoviesSaga.ts
@@ -1,12 +1,20 @@
-import { put, takeEvery, call } from 'redux-saga/effects';
-import { MoviesTypes } from '../constants';
-import TMDB, { FetchMoviesResponse } from '../../../api';
 import {
+  put as untypedPut,
+  takeEvery,
+  call,
+  PutEffect,
+} from 'redux-saga/effects';
+import {
+  MoviesActionTypes,
   fetchMoviesStart,
   fetchMoviesSuccess,
   RequestMoviesAction,
   fetchMoviesError,
 } from '../actions';
+import { MoviesTypes } from '../constants';
+import TMDB, { FetchMoviesResponse } from '../../../api';
+
+const put = <A extends MoviesActionTypes>(action: A): PutEffect<A> => untypedPut(action);
 
 export function* fetchMovies({ page }: RequestMoviesAction) {
   try {
