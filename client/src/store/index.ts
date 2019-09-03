485a3871e71833ae5bd9bbd0b534d702102d7a9c
Asaf Aviv <asafaviv89@gmail.com>
2019-09-03 22:39:08 +0300

    added shows reducer

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import notificationsReducer from './notifications/reducers';
-import moviesReducer from './movies/reducers';
 import configureStore from './configureStore';
+import moviesReducer from './movies/reducers';
+import showsReducer from './shows/reducers';
 
 const rootReducer = combineReducers({
   notifications: notificationsReducer,
   movies: moviesReducer,
+  shows: showsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
