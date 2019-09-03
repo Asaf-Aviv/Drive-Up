00c65c4ef580505b0a37766eed2475607a9b6a0d
Asaf Aviv <asafaviv89@gmail.com>
2019-09-03 22:37:56 +0300

    added method to fetch shows and person

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,30 +1,43 @@
 import axios from 'axios';
+import { ShowShowcase } from '../store/shows/interfaces';
 import { MovieShowcase } from '../store/movies/interfaces';
+import { Person } from '../store/people/interfaces';
 
-export interface FetchMoviesResponse {
+export interface BaseResponse<T> {
   data: {
+    results: T[];
     page: number;
-    results: MovieShowcase[];
     total_results: number;
     total_pages: number;
   };
 }
 
-class TmdbApi {
+class TheMovieDB {
   private fetcher = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
       api_key: process.env.REACT_APP_API_KEY as string,
     },
-  })
-
-  fetchMovies = (page: number) => this.fetcher.get<FetchMoviesResponse>('/discover/movie', {
-    params: {
-      page,
-    },
   });
+
+  fetchMovies = (page: number) =>
+    this.fetcher.get<BaseResponse<MovieShowcase>>('/discover/movie', {
+      params: {
+        page,
+      },
+    });
+
+  fetchShows = (page: number) =>
+    this.fetcher.get<BaseResponse<ShowShowcase>>('/discover/tv', {
+      params: {
+        page,
+      },
+    });
+
+  fetchPerson = (personId: string) =>
+    this.fetcher.get<Person>(`/persons/${personId}`);
 }
 
-const TMDB = new TmdbApi();
+const TMDB = new TheMovieDB();
 
 export default TMDB;
