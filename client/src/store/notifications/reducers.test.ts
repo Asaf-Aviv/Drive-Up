dcd46714d1dd0cc361dc1f1bac2b23c01edd9415
Asaf Aviv <asafaviv89@gmail.com>
2019-09-06 13:18:56 +0300

    added tests

--- client/src/store/notifications/reducers.test.ts
+++ client/src/store/notifications/reducers.test.ts
@@ -11,7 +11,7 @@ describe('notification reducer', () => {
     );
   });
 
-  it('should hande ADD_NOTIFICATION', () => {
+  it('should handle ADD_NOTIFICATION', () => {
     const state: Notification[] = [];
 
     const notification: Notification = {
@@ -42,6 +42,22 @@ describe('notification reducer', () => {
       { id: '1', message: 'hello', variant: 'success' },
       { id: '3', message: 'yallow', variant: 'warning' },
     ]);
+  });
+
+  it('REMOVE_NOTIFICATION should return the current state if id is not found', () => {
+    const state: Notification[] = [
+      { id: '1', message: 'hello', variant: 'success' },
+      { id: '2', message: 'hi', variant: 'info' },
+      { id: '3', message: 'yallow', variant: 'warning' },
+    ];
+
+    const action = removeNotification('4');
+
+    expect(
+      notificationsReducer(state, action)
+    ).toEqual(
+      state
+    );
   });
 });
 
