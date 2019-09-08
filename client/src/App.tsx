95b26818dad544b0ebc09795843ce5f1a710aa5d
Asaf Aviv <asafaviv89@gmail.com>
2019-09-08 23:55:43 +0300

    refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { useDispatch } from 'react-redux';
-import useShallowEqualSelector from './hooks/useShallowEqualSelector';
-import { requestMovies } from './store/movies/actions';
-import { useOnVisibilityTrigger } from './hooks/useOnVisibilityTrigger';
+import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme } from '@material-ui/core';
+import Routes from './components/Routes';
 
-const App: React.FC = () => {
-  const dispatch = useDispatch();
-  const { page, loading, movies } = useShallowEqualSelector(state => state.movies);
-  const triggerRef = useRef(null);
-  useOnVisibilityTrigger(triggerRef, () => dispatch(requestMovies(page + 1)));
+const theme = createMuiTheme();
 
-  useEffect(() => {
-    dispatch(requestMovies(1));
-  }, [dispatch]);
+const App: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Routes />
+  </ThemeProvider>
+);
 
-  return (
-    <div className="App">
-      <CssBaseline />
-      {movies.length > 0 && (
-        <div>
-          {movies.map(movie => (
-            <div key={movie.id}>
-              <h3>{movie.id}</h3>
-              <h3>{movie.title}</h3>
-              <p>{movie.original_language}</p>
-              <p>{movie.overview}</p>
-              <img
-                src={`http://image.tmdb.org/t/p/w92/${movie.poster_path}`}
-                alt={movie.title}
-              />
-            </div>
-          ))}
-          {loading ? 'Loading...' : <span ref={triggerRef} />}
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default App;
+export default hot(App);
