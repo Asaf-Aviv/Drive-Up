9ed0de98a4e1e6dc1a054bea8412cce7ef2e0b27
Asaf Aviv <asafaviv89@gmail.com>
2019-09-09 04:53:46 +0300

    added tests

--- client/src/components/MovieShowcase/MovieShowcase.test.tsx
+++ client/src/components/MovieShowcase/MovieShowcase.test.tsx
@@ -1,9 +1,7 @@
-import React, { FunctionComponent } from 'react';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
 import { mount } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
 import MovieShowcase from './MovieShowcase';
 import { MovieShowcase as IMovieShowcase } from '../../store/movies/interfaces';
+import { renderWithProviders } from '../../test-utils';
 
 const movie: IMovieShowcase = {
   adult: false,
@@ -22,29 +20,37 @@ const movie: IMovieShowcase = {
   vote_count: 135,
 };
 
-const renderWithProviders = (Component: FunctionComponent<any>, props = {}) => (
-  <MemoryRouter>
-    <MuiThemeProvider theme={createMuiTheme()}>
-      <Component {...props} />
-    </MuiThemeProvider>
-  </MemoryRouter>
-);
-
 describe('MovieShowcase component', () => {
   it('renders correctly', () => {
     const wrapper = mount(renderWithProviders(MovieShowcase, movie));
     expect(wrapper.debug()).toMatchSnapshot();
   });
 
-  it('should display the title of the movie', () => {
+  it('should render the title of the movie', () => {
     const wrapper = mount(renderWithProviders(MovieShowcase, movie));
     const title = wrapper.find('h2').text();
     expect(title).toEqual(movie.title);
   });
 
-  it('should have a link to the movie by id ', () => {
+  it('should render the overview of the movie', () => {
+    const wrapper = mount(renderWithProviders(MovieShowcase, movie));
+    const title = wrapper.find('h6').text();
+    expect(title).toEqual(movie.overview);
+  });
+
+  it('should render a link to the movie by id', () => {
     const wrapper = mount(renderWithProviders(MovieShowcase, movie));
     const href = wrapper.find('a').prop('href');
     expect(href).toEqual(`/movie/${movie.id}`);
+  });
+
+  it('should render the movie image', () => {
+    const wrapper = mount(renderWithProviders(MovieShowcase, movie));
+    const imgSrc = wrapper.find('img').prop('src');
+    const baseSrc = 'http://image.tmdb.org/t/p/w';
+
+    expect(wrapper.exists('img')).toBeTruthy();
+    expect(imgSrc).toContain(baseSrc);
+    expect(imgSrc).toContain(movie.poster_path);
   });
 });
