488af8ee297ef459f3ed3f1f573aed0cfe6e44d8
Asaf Aviv <asafaviv89@gmail.com>
2019-09-09 21:01:28 +0300

    type currentRef

--- client/src/hooks/useOnVisibilityTrigger.ts
+++ client/src/hooks/useOnVisibilityTrigger.ts
@@ -3,8 +3,6 @@ import { useEffect } from 'react';
 /**
  * a hook that fires the callback function when the ref element is 500px
  * below the current scroll position
- * @param {...React.RefObject} ref React.ref object
- * @param {callback} callback a function to be executed
  */
 
 export function useOnVisibilityTrigger(ref: React.RefObject<HTMLElement>, callback: () => void) {
@@ -12,7 +10,7 @@ export function useOnVisibilityTrigger(ref: React.RefObject<HTMLElement>, callba
     let observer: IntersectionObserver;
     // capture ref.current because rec.current might
     // change before the cleanup function runs
-    let currentRef: typeof ref.current | null = null;
+    let currentRef: HTMLElement | null = null;
 
     if (ref.current) {
       currentRef = ref.current;
