ca19c52d643cfc51cec8dd45e5b6f01126c08440
Asaf Aviv <asafaviv89@gmail.com>
2019-09-11 21:43:05 +0300

    added requestPersonsWatcher

--- client/src/store/sagas/rootSaga.ts
+++ client/src/store/sagas/rootSaga.ts
@@ -2,9 +2,11 @@ import { fork } from 'redux-saga/effects';
 import { requestMoviesWatcher } from '../movies/sagas/fetchMovies';
 import { requestShowsWatcher } from '../shows/sagas/fetchShows';
 import { notificationsWatcher } from '../notifications/sagas/notificationsSaga';
+import { requestPersonsWatcher } from '../persons/sagas/fetchPersons';
 
 export default function* rootSaga() {
   yield fork(requestMoviesWatcher);
   yield fork(requestShowsWatcher);
+  yield fork(requestPersonsWatcher);
   yield fork(notificationsWatcher);
 }
