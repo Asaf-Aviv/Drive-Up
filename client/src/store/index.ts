13e8d28846235d9584da65208dee457380828f75
Asaf Aviv <asafaviv89@gmail.com>
2019-09-12 03:32:02 +0300

    added persons reducer

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -3,11 +3,13 @@ import notificationsReducer from './notifications/reducers';
 import configureStore from './configureStore';
 import moviesReducer from './movies/reducers';
 import showsReducer from './shows/reducers';
+import personsReducer from './persons/reducers';
 
 export const rootReducer = combineReducers({
   notifications: notificationsReducer,
   movies: moviesReducer,
   shows: showsReducer,
+  persons: personsReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
