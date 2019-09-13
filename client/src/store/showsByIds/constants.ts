461a3a945b8f2310d2c37142a69f2e3a0632dd8a
Asaf Aviv <asafaviv89@gmail.com>
2019-09-13 04:23:53 +0300

    ShowsByIds

---
+++ client/src/store/showsByIds/constants.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-multi-spaces */
+export enum ShowsByIdsTypes {
+  REQUEST_SHOW_BY_ID       = 'REQUEST_SHOW_BY_ID',
+  FETCH_SHOW_BY_ID_START   = 'FETCH_SHOW_BY_ID_START',
+  FETCH_SHOW_BY_ID_SUCCESS = 'FETCH_SHOW_BY_ID_SUCCESS',
+  FETCH_SHOW_BY_ID_ERROR   = 'FETCH_SHOW_BY_ID_ERROR',
+}
+/* eslint-enable no-multi-spaces */
