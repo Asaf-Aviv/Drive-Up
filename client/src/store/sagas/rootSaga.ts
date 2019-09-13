c2c7b216e819453c58fa6597aa731a6ecfe8b88f
Asaf Aviv <asafaviv89@gmail.com>
2019-09-13 04:24:40 +0300

    added sagas

--- client/src/store/sagas/rootSaga.ts
+++ client/src/store/sagas/rootSaga.ts
@@ -3,10 +3,16 @@ import { requestMoviesWatcher } from '../movies/sagas/fetchMovies';
 import { requestShowsWatcher } from '../shows/sagas/fetchShows';
 import { notificationsWatcher } from '../notifications/sagas/notificationsSaga';
 import { requestPersonsWatcher } from '../persons/sagas/fetchPersons';
+import { requestMovieByIdWatcher } from '../moviesByIds/sagas/fetchMovieById';
+import { requestShowByIdWatcher } from '../showsByIds/sagas/fetchShowById';
+import { requestPersonByIdWatcher } from '../personsByIds/sagas/fetchPersonById';
 
 export default function* rootSaga() {
   yield fork(requestMoviesWatcher);
+  yield fork(requestMovieByIdWatcher);
   yield fork(requestShowsWatcher);
+  yield fork(requestShowByIdWatcher);
   yield fork(requestPersonsWatcher);
+  yield fork(requestPersonByIdWatcher);
   yield fork(notificationsWatcher);
 }
