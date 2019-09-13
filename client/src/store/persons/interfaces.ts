a6a2c064812db21f3514345a5a6968d09ebbd769
Asaf Aviv <asafaviv89@gmail.com>
2019-09-13 04:26:29 +0300

    moved Person interface to a different file

--- client/src/store/persons/interfaces.ts
+++ client/src/store/persons/interfaces.ts
@@ -1,20 +1,3 @@
-export interface Person {
-  birthday: string | null;
-  known_for_department: string;
-  deathday: string | null;
-  id: number;
-  name: string;
-  also_known_as: string[];
-  gender: 0 | 1 | 2;
-  biography: string;
-  popularity: number;
-  place_of_birth: string | null;
-  profile_path: string | null;
-  adult: boolean;
-  imdb_id: string;
-  homepage: string | null;
-}
-
 export interface PersonSummary {
   profile_path: string;
   adult: boolean;
