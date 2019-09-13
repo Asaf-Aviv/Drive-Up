a3e5d67d63f2eb3ff2792be70f2d5090de3737e7
Asaf Aviv <asafaviv89@gmail.com>
2019-09-13 04:26:53 +0300

    update tests

--- client/src/store/sagas/rootSaga.test.ts
+++ client/src/store/sagas/rootSaga.test.ts
@@ -4,14 +4,20 @@ import { requestMoviesWatcher } from '../movies/sagas/fetchMovies';
 import { requestShowsWatcher } from '../shows/sagas/fetchShows';
 import { notificationsWatcher } from '../notifications/sagas/notificationsSaga';
 import { requestPersonsWatcher } from '../persons/sagas/fetchPersons';
+import { requestMovieByIdWatcher } from '../moviesByIds/sagas/fetchMovieById';
+import { requestShowByIdWatcher } from '../showsByIds/sagas/fetchShowById';
+import { requestPersonByIdWatcher } from '../personsByIds/sagas/fetchPersonById';
 
 describe('rootSaga', () => {
   it('should react to actions', () => {
     const gen = rootSaga();
 
     expect(gen.next().value).toEqual(fork(requestMoviesWatcher));
+    expect(gen.next().value).toEqual(fork(requestMovieByIdWatcher));
     expect(gen.next().value).toEqual(fork(requestShowsWatcher));
+    expect(gen.next().value).toEqual(fork(requestShowByIdWatcher));
     expect(gen.next().value).toEqual(fork(requestPersonsWatcher));
+    expect(gen.next().value).toEqual(fork(requestPersonByIdWatcher));
     expect(gen.next().value).toEqual(fork(notificationsWatcher));
     expect(gen.next().done).toBeTruthy();
   });
