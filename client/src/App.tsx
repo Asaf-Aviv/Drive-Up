a2e4b20c9390cfe2f8a80a4a045f9d0d47fbe546
Asaf Aviv <asafaviv89@gmail.com>
2019-09-20 08:32:31 +0300

    Moved materiall ui to index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme } from '@material-ui/core';
 import Routes from './components/Routes';
 import NavBar from './components/NavBar';
 
-const theme = createMuiTheme();
-
 const App: React.FC = () => (
-  <ThemeProvider theme={theme}>
+  <>
     <CssBaseline />
     <NavBar />
     <Routes />
-  </ThemeProvider>
+  </>
 );
 
 export default hot(App);
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,8 +5,27 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import ReactGA from 'react-ga';
+import { createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 import App from './App';
 import store from './store';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ff2121',
+    },
+    secondary: {
+      main: '#faff21',
+    },
+    type: 'dark',
+  },
+  props: {
+    MuiButton: {
+      variant: 'contained',
+    },
+  },
+});
 
 ReactGA.initialize('');
 
@@ -20,7 +39,9 @@ history.listen((location) => {
 render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Router>
   </Provider>,
   document.getElementById('root'),
