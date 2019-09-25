764bc629f6f33d5586b089250383f53afd4f108e
Asaf Aviv <asafaviv89@gmail.com>
2019-09-25 13:36:05 +0300

    utils

---
+++ client/src/utils/index.ts
@@ -0,0 +1,19 @@
+import genres from '../data/genres';
+
+export const minutesConverter = (minutes: number) => {
+  if (minutes < 60) {
+    return `${minutes} minutes`;
+  }
+
+  const hours = Math.floor(minutes / 60);
+  const mins = minutes % 60;
+
+  const hoursPhrase = hours > 1 ? `${hours} hours` : '1 hour';
+
+  if (!mins) return hoursPhrase;
+
+  return `${hoursPhrase} and ${mins > 1 ? `${mins} minutes` : '1 minute'}`;
+};
+
+export const genreIdToString = (genreIds: number[]) =>
+  genreIds.map(id => genres[id]);
