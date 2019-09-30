de390f26645657033e80fede8fb19c6b4d05abce
Asaf Aviv <asafaviv89@gmail.com>
2019-10-01 02:46:01 +0300

    PersonList

--- client/src/components/PersonsList/PersonsList.tsx
+++ client/src/components/PersonsList/PersonsList.tsx
@@ -1,44 +1,48 @@
 import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { List, ListItem } from '@material-ui/core';
+import { Grid, Container } from '@material-ui/core';
 import useShallowEqualSelector from '../../hooks/useShallowEqualSelector';
 import { useOnVisibilityTrigger } from '../../hooks/useOnVisibilityTrigger';
-import { PersonSummary as IPersonSummary } from '../../store/persons/interfaces';
+import { PersonSummary } from '../../store/persons/interfaces';
 import RetryButton from '../RetryButton';
-import { requestPersons } from '../../store/persons/actions';
-import PersonShowcase from '../PersonShowcase';
+import { requestPopularPersons } from '../../store/persons/actions';
+import CelebTile from '../CelebTile';
 
 const PersonsList: React.FC = () => {
   const {
-    page, loading, persons, error,
-  } = useShallowEqualSelector(state => state.persons);
+    page, loading, results, error,
+  } = useShallowEqualSelector(state => state.persons.popular);
   const dispatch = useDispatch();
   const fetchNextPageTrigger = useRef<HTMLElement>(null);
 
   const fetchNextPersonsPage = () => {
-    if (persons.length) return;
-
-    dispatch(requestPersons(page + 1));
+    dispatch(requestPopularPersons(page + 1));
   };
 
   useEffect(fetchNextPersonsPage, []);
 
   useOnVisibilityTrigger(fetchNextPageTrigger, fetchNextPersonsPage);
 
-  const renderPersonSummary = (person: IPersonSummary) => (
-    <ListItem key={person.id}>
-      <PersonShowcase {...person} />
-    </ListItem>
+  const renderPersonSummary = (person: PersonSummary) => (
+    <Grid item xs={3} key={person.id}>
+      <CelebTile
+        id={person.id}
+        name={person.name}
+        image={person.profile_path}
+      />
+    </Grid>
   );
 
   return (
     <main>
-      <List>
-        {persons.map(renderPersonSummary)}
-        {!error && !loading && <span ref={fetchNextPageTrigger} />}
-        {loading && <span>Loading...</span>}
-        {error && <RetryButton onClick={fetchNextPersonsPage} />}
-      </List>
+      <Container>
+        <Grid container spacing={3}>
+          {results.map(renderPersonSummary)}
+          {!error && !loading && <span ref={fetchNextPageTrigger} />}
+          {loading && <span>Loading...</span>}
+          {error && <RetryButton onClick={fetchNextPersonsPage} />}
+        </Grid>
+      </Container>
     </main>
   );
 };
