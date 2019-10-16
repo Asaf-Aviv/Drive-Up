2f3edeea34939525a76817e127f2285e7622443f
Asaf Aviv <asafaviv89@gmail.com>
2019-10-16 20:54:17 +0300

    added routes

--- client/src/components/Routes/Routes.tsx
+++ client/src/components/Routes/Routes.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router';
-import MovieShowcaseList from '../MovieShowcaseList';
+import PersonsList from '../PersonsList';
+import Movie from '../Movie';
+import Show from '../Show';
+import Person from '../Person';
+import MoviesByCategory from '../MoviesByCategory';
+import MoviesByQuery from '../MoviesByQuery';
+import ShowsByCategory from '../ShowsByCategory';
+import ShowsByQuery from '../ShowsByQuery';
+import Home from '../Home';
+import ShowSeason from '../../ShowSeason';
 
-const Routes = () => (
+const Routes: React.FC = () => (
   <Switch>
-    <Route exact path="/" component={MovieShowcaseList} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/movies" component={MoviesByQuery} />
+    <Route path="/movies/:category" component={MoviesByCategory} />
+    <Route path="/movie/:movieId" component={Movie} />
+    <Route exact path="/shows" component={ShowsByQuery} />
+    <Route path="/shows/:category" component={ShowsByCategory} />
+    <Route exact path="/show/:showId" component={Show} />
+    <Route path="/show/:showId/season/:seasonNumber" component={ShowSeason} />
+    <Route path="/persons" component={PersonsList} />
+    <Route path="/person/:personId" component={Person} />
     <Route render={() => <Redirect to="/" />} />
   </Switch>
 );
