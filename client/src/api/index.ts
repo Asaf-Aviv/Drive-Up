1ead1c1c3d8842e799a379687fc989c28fa449a7
Asaf Aviv <asafaviv89@gmail.com>
2019-10-18 19:18:32 +0300

    added api methods

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { Show } from '../store/showsByIds/interfaces';
-import { Movie } from '../store/moviesByIds/interfaces';
-import { ShowShowcase } from '../store/shows/interfaces';
-import { MovieShowcase } from '../store/movies/interfaces';
-import { PersonSummary } from '../store/persons/interfaces';
-import { Person } from '../store/personsByIds/interfaces';
+import { Movie, MovieShowcase } from '../store/movies/interfaces';
+import { RelatedFields } from '../store/movies/actions';
+import { Show, ShowShowcase } from '../store/shows/interfaces';
+import { PersonSummary, Person } from '../store/persons/interfaces';
+import { Collection } from '../store/collections/interfaces';
 
 export interface BaseResponse<T> {
   data: {
@@ -15,7 +14,18 @@ export interface BaseResponse<T> {
   };
 }
 
+interface MovieImage {
+  file_path: string;
+}
+
+interface ImagesResponse {
+  backdrops: MovieImage[];
+  posters: MovieImage[];
+}
+
 class TheMovieDB {
+  imgURL = 'https://image.tmdb.org/t/p';
+
   private fetcher = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     params: {
@@ -23,37 +33,110 @@ class TheMovieDB {
     },
   });
 
-  fetchMovies = (page: number) =>
-    this.fetcher.get<BaseResponse<MovieShowcase>>('/movie', {
+  search = (category: string, params: any, page: number) =>
+    this.fetcher.get(`/search/${category}`, {
+      params: { ...params, page },
+    });
+
+  fetchMoviesByQuery = (params: any, page: number) =>
+    this.fetcher.get<BaseResponse<MovieShowcase>>('/discover/movie', {
+      params: { ...params, page },
+    });
+
+  fetchShowsByQuery = (params: any, page: number) =>
+    this.fetcher.get<BaseResponse<MovieShowcase>>('/discover/tv', {
+      params: { ...params, page },
+    });
+
+  fetchShowSeason = (showId: number, seasonNumber: number) =>
+    this.fetcher.get(`/tv/${showId}/season/${seasonNumber}`);
+
+  fetchMovieImages = (movieId: number | string) =>
+    this.fetcher
+      .get<ImagesResponse>(`/movie/${movieId}/images`)
+      .then(({ data }) => {
+        const mapImages = ({ file_path, width, height }: any) => ({
+          src: `https://image.tmdb.org/t/p/original/${file_path}`,
+          width,
+          height,
+        });
+        const posters = data.posters.map(mapImages);
+        const backdrops = data.backdrops.map(mapImages);
+        return {
+          posters,
+          backdrops,
+        };
+      });
+
+  fetchMovieById = (movieId: string | number) =>
+    this.fetcher.get<Movie>(`/movie/${movieId}`, {
+      params: {
+        append_to_response: 'images,similar,recommendations,videos,credits',
+      },
+    });
+
+  fetchRelatedMovies = (
+    movieId: number,
+    relatedField: RelatedFields,
+    page: number
+  ) =>
+    this.fetcher.get(`/movie/${movieId}/${relatedField}`, { params: { page } });
+
+  fetchRelatedShows = (
+    showId: number,
+    relatedField: RelatedFields,
+    page: number
+  ) => this.fetcher.get(`/tv/${showId}/${relatedField}`, { params: { page } });
+
+  fetchMoviesByCategory = (category: string, page: number) =>
+    this.fetcher.get<BaseResponse<MovieShowcase>>(`/movie/${category}`, {
       params: {
         page,
       },
     });
 
-  fetchMovieById = (movieId: number) =>
-    this.fetcher.get<Movie>(`/movie/${movieId}`);
-
-  fetchShows = (page: number) =>
-    this.fetcher.get<BaseResponse<ShowShowcase>>('/discover/tv', {
+  fetchShowsByCategory = (category: string, page: number) =>
+    this.fetcher.get<BaseResponse<ShowShowcase>>(`/tv/${category}`, {
       params: {
         page,
       },
     });
 
-  fetchShowById = (showId: number) =>
-    this.fetcher.get<Show>(`/show/${showId}`);
+  fetchShowById = (showId: number | string) =>
+    this.fetcher.get<Show>(`/tv/${showId}`, {
+      params: {
+        append_to_response: 'images,similar,recommendations,videos',
+      },
+    });
 
-  fetchPersons = (page: number) =>
+  fetchPopularPersons = (page: number) =>
     this.fetcher.get<BaseResponse<PersonSummary>>('/person/popular', {
       params: {
         page,
       },
     });
 
-  fetchPersonById = (personId: number) =>
-    this.fetcher.get<Person>(`/person/${personId}`);
+  fetchPersonById = (personId: number | string) =>
+    this.fetcher.get<Person>(`/person/${personId}`, {
+      params: {
+        append_to_response: 'images,movie_credits,tv_credits,videos',
+      },
+    });
+
+  fetchMovieCollection = (collectionId: number | string) =>
+    this.fetcher.get<Collection>(`/collection/${collectionId}`)
 }
 
 const TMDB = new TheMovieDB();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.TMDB = TMDB;
+}
+
+declare global {
+  interface Window {
+    TMDB: TheMovieDB;
+  }
+}
 
 export default TMDB;
