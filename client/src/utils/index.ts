5e2e09d6a8c2b2d644a0a79c6ad6dc7875fe766d
Asaf Aviv <asafaviv89@gmail.com>
2019-10-23 23:23:23 +0300

    utils

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -2,17 +2,27 @@ import genres from '../data/genres';
 
 export const minutesConverter = (minutes: number) => {
   if (minutes < 60) {
-    return `${minutes} minutes`;
+    return `${minutes} mins`;
   }
 
   const hours = Math.floor(minutes / 60);
   const mins = minutes % 60;
 
-  const hoursPhrase = hours > 1 ? `${hours} hours` : '1 hour';
+  const hoursPhrase = hours > 1 ? `${hours} hrs` : '1 hr';
 
   if (!mins) return hoursPhrase;
 
-  return `${hoursPhrase} and ${mins > 1 ? `${mins} minutes` : '1 minute'}`;
+  return `${hoursPhrase} ${mins} ${mins > 1 ? 'mins' : 'min'}`;
 };
 
 export const genreIdToString = (genreId: number) => genres[genreId];
+
+const dateOptions = {
+  year: 'numeric', month: 'long', day: 'numeric',
+};
+
+export const formatDate = (date: any) => new Date(date).toLocaleDateString('en-US', dateOptions);
+
+export const getImgUrl = (imgPath: string, width = 185) => `
+https://image.tmdb.org/t/p/w${width}/${imgPath}
+`;
