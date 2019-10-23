457f8bdc19e5bd95ef706a4bf3801b9d8e71dfd4
Asaf Aviv <asafaviv89@gmail.com>
2019-10-23 23:23:39 +0300

    styled-components theme

---
+++ client/src/themes/index.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#642caf',
+    secondary: '#fbff07',
+  },
+  textColors: {
+    primary: '#FFF',
+    secondary: '#989898',
+  },
+  button: {
+    size: {
+      small: '20px',
+      normal: '35px',
+      big: '40px',
+      huge: '65px',
+    },
+  },
+};
+
+
+export default theme;
