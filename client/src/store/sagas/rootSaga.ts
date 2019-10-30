349a7a64737c9391e8b054ee83e8f11db96c2b66
Asaf Aviv <asafaviv89@gmail.com>
2019-10-30 23:16:42 +0200

    fork requestTrendingWatcher

--- client/src/store/sagas/rootSaga.ts
+++ client/src/store/sagas/rootSaga.ts
@@ -6,8 +6,10 @@ import { requestPersonsWatcher } from '../persons/sagas';
 import { requestSearchResultsWatcher } from '../search/sagas';
 import { requestSeasonsWatcher } from '../seasons/sagas';
 import { requestCollectionWatcher } from '../collections/sagas';
+import { requestTrendingWatcher } from '../trending/sagas';
 
 export default function* rootSaga() {
+  yield fork(requestTrendingWatcher);
   yield fork(requestMoviesWatcher);
   yield fork(requestCollectionWatcher);
   yield fork(requestShowsWatcher);
