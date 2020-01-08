a34b155df7a0c0696468aa1034cb69f6f34317d1
Asaf Aviv <asafaviv89@gmail.com>
2020-01-08 19:41:51 +0200

    added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import { App } from 'components'
 import { BrowserRouter } from 'react-router-dom'
+import * as Sentry from '@sentry/browser'
+
+Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN })
 
 render(
   <BrowserRouter>
